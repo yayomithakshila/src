94aedafb327b3e99c4e9f1bbdca30c9ec68d722b
Yayomi Thakshila Premathilaka <yayomifin@outlook.com>
2024-02-07 22:36:25 +0200

    exercise7

--- yayomiytp_tasks/crud/read.php
+++ yayomiytp_tasks/crud/read.php
@@ -30,7 +30,8 @@ if ($result->num_rows > 0) {
     // Loop through the result set and display data in rows
     while ($row = $result->fetch_assoc()) {
         echo "<tr>
-                <td>{$row['id']}</td>
+                <td><a href='updatesingle.php?id=$row[id]'styles='color:red;'>$row[id]</a></td>
+                
                 <td>{$row['first_name']}</td>
                 <td>{$row['last_name']}</td>
                 <td>{$row['city']}</td>
