bc2a689bf75f43be06b05f678ea6ddb3908892a8
Yayomi Thakshila Premathilaka <yayomifin@outlook.com>
2024-02-08 13:22:07 +0200

    updated

---
+++ yayomiytp_tasks/Ex2/javascript.php
@@ -0,0 +1,38 @@
+<?php 
+ 
+ include 'header.php'; ?> 
+ 
+ 
+
+
+<h2> About us </h2>
+
+<script>
+    document.write("Hello World")
+   // window.alert("This is an alert box")
+
+
+</script>
+
+
+
+        
+
+<noscript>
+    If javascript is not enable this will be displayed.so users know they want to enable it.
+</noscript>
+       
+
+<script>
+    //document .write but after loading the page
+    function hello() {
+        document.write("This is after the page has loaded")
+    }
+</script>
+    <button onclick="document.write('This is something')"> Click Me</button>
+
+
+            
+       
+<?php include 'footer.php'; ?>
+        
